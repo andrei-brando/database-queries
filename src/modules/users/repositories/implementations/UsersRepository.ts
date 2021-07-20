0eeda1d8e80f2571a3192031b13253413205c71c
andrei-brando <albertbrando@hotmail.com>
2021-07-20 08:52:32 -0300

    completa repositório de users

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,29 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const userWithGames = await this.repository.findOne(
+      user_id,
+      {
+        relations: ['games'],
+      }
+    );
+
+    return userWithGames!;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(
+      'select * from users order by first_name'
+    ); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(
+      'select * from users u where lower(u.first_name) = lower($1) and lower(u.last_name) = lower($2)',
+      [first_name, last_name,],
+    ); // Complete usando raw query
   }
 }
