921c88e0ed60cdf767063352c6aa21acb5af3549
andrei-brando <albertbrando@hotmail.com>
2021-07-20 08:52:20 -0300

    completa repositório de games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,36 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games = await this.repository
+      .createQueryBuilder('g')
+      .where('lower(g.title) ilike(:title)', { title: `%${param}%` })
+      .getMany();
+
+    return games;
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('select count(1) from games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const response = await this.repository
+      .createQueryBuilder('g')
+      .leftJoinAndSelect('users_games_games', 'user_games', 'user_games.gamesId = g.id')
+      .leftJoinAndSelect('users', 'user', 'user.id = user_games.usersId')
+      .where('g.id = :id', { id })
+      .getRawMany()
+
+    const users = (response.map((gameUsers) => {
+      return {
+        first_name: gameUsers['user_first_name'],
+        last_name: gameUsers['user_last_name'],
+        email: gameUsers['user_email'],
+      }
+    })) as User[];
+
+    return users;
+    // Complete usando query builder
   }
 }
